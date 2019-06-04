11f431b6b58bc3f32b745befe61532436caf108c
JRod166 <jose.vilca.vargas@gmail.com>
2019-06-04 10:36:22 -0500

    beat track

--- Snippet.py
+++ Snippet.py
@@ -15,17 +15,34 @@ N_MFCCS=12
 
 #audio_files to be extracted
 audio_file = ["digital_love.mp3", "pequenas_cosas.mp3","heartbeat.mp3","mindless.ogg","sines.ogg"]
+#audio_file = ["sines.ogg"]
+
+def normalize(X):
+    X += np.abs(X.min())
+    X /= X.max()
+    return X
+
+
+def compute_beats(y, sr,oe):
+
+    logging.info("Estimating Beats...")
+    tempo, beats_idx = librosa.beat.beat_track(y=y, sr=sr,
+                                               hop_length=HOP_SIZE, onset_envelope=oe)
+    return beats_idx, librosa.frames_to_time(np.arange(len(oe)), sr=sr,
+                                             hop_length=HOP_SIZE)
 
 
 
-def process_file(file):
+def process_features(file):
 
+    plt.figure(figsize=(12, 16))
     #Load audio
     try:
         logging.info("Loading file {}".format(os.path.basename(file)))
         audio, sr=librosa.load(file,sr=SAMPLING_RATE)
-    except:
+    except Exeption as e:
         logging.info("Couldn't load file {}".format(os.path.basename(file)))
+        logging.info("Error: {}".format(e))
         return
 
     #Harmonic and Precusive spectrograms
@@ -38,7 +55,6 @@ def process_file(file):
 
         D_harmonic, D_percussive = librosa.decompose.hpss(D)
         rp = np.max(np.abs(D))
-        plt.figure(figsize=(12, 16))
         plt.subplot(4, 2, 1)
         librosa.display.specshow(librosa.amplitude_to_db(np.abs(D), ref=rp), y_axis='log')
         plt.colorbar()
@@ -60,11 +76,6 @@ def process_file(file):
 
         logging.info("Percussive spectrogram: done")
 
-    except:
-        logging.info("Failed to generate spectrograms for {}".format(os.path.basename(file)))
-    try:
-        logging.info("Computing MFCC spectrogram")
-
         features={}
         S= librosa.feature.melspectrogram(audio,sr=sr,n_fft=FFT_SIZE, hop_length=HOP_SIZE, n_mels=N_MELS)
         plt.subplot(4, 2, 4)
@@ -73,13 +84,15 @@ def process_file(file):
         plt.colorbar()
         plt.title('MFCC spectrogram')
 
-        logging.info("Generated spectrograms for {}".format(os.path.basename(file)))
+        logging.info("MFCC spectrogram: done")
 
-        features["sequence"] =librosa.feature.mfcc(S=log_S, n_mfcc=N_MFCCS).T
+        features["sequence"] =librosa.feature.mfcc(S=log_S, n_mfcc=N_MFCCS)
         plt.subplot(4,2,5)
         librosa.display.specshow(features["sequence"],x_axis="time")
         plt.colorbar()
         plt.title('MFCC spectrogram')
+
+        logging.info("MFCC spectrogram: done")
 
         chroma=librosa.feature.chroma_cens(y=audio,sr=sr)
         plt.subplot(4,2,6)
@@ -87,12 +100,40 @@ def process_file(file):
         plt.colorbar()
         plt.title('Chroma Spectrogram')
 
-        plt.savefig("{}.png".format(file), box_inches='tight')
+        logging.info("Chroma spectrogram: done")
+
+        #Normalize sequence
+        features["sequence"]=normalize(features["sequence"])
+        plt.subplot(4,2,7)
+        librosa.display.specshow(features["sequence"],x_axis="time")
+        plt.colorbar()
+        plt.title('MFCC normalized')
+
+        logging.info("MFCC normalized spectrogram: done")
+        logging.info("Generated spectrograms for {}".format(os.path.basename(file)))
+
     except Exception as e:
-        raise
+        logging.info("Failed to generate spectrograms for {}".format(os.path.basename(file)))
+        logging.info("Error: {}".format(e))
 
+    try:
+        onset_env = librosa.onset.onset_strength(audio, sr=sr,aggregate=np.median)
+        features["beats_idx"], times = compute_beats(audio,sr=sr,oe=onset_env)
+        features["beats"]=times;
+        plt.subplot(4,2,8)
+        plt.plot(times,librosa.util.normalize(onset_env),label='Onset Strength')
+        plt.vlines(times[features["beats_idx"]],0,1,alpha=0.5,color='r',linestyle='--',label='Beats')
+        plt.legend(frameon=True, framealpha=0.75)
+        plt.gca().xaxis.set_major_formatter(librosa.display.TimeFormatter())
+        plt.title('Beat track')
+        logging.info("Generated beat-track")
 
+    except Exception as e:
+        logging.info("Failed Beat Estimation")
+        logging.info("Error: {}".format(e))
+
+    plt.savefig("{}.png".format(file), box_inches='tight')
 #Setup logger
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
 for item in audio_file:
-    process_file(item)
+    process_features(item)
